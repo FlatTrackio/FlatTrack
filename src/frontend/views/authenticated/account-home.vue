a596747301453cf90e72571cd720837e043c29e2
Caleb Woodbine <calebwoodbine.public@gmail.com>
2020-04-23 18:59:59 +1200

    Add missing account pages

---
+++ src/frontend/views/authenticated/account-home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="container">
+      <section class="section">
+        <h1 class="title is-1">My Account</h1>
+        <p class="subtitle is-3">Manage your account</p>
+        <p></p>
+        <br>
+        <div v-for="app in apps" v-bind:key="app" >
+          <div class="card pointer-cursor-on-hover" @click="goToApp(app.ref)">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <b-icon :icon="app.icon" size="is-medium"></b-icon>
+                </div>
+                <div class="media-content">
+                  <p class="title is-3">{{ app.name }}</p>
+                  <p class="subtitle is-5">{{ app.description }}</p>
+                </div>
+                <div class="media-right">
+                  <b-icon icon="chevron-right" size="is-medium" type="is-midgray"></b-icon>
+                </div>
+              </div>
+            </div>
+            <div class="content">
+            </div>
+          </div>
+          <br />
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Account home',
+  data () {
+    return {
+      apps: [
+        {
+          name: 'Profile',
+          description: 'Manage your general information',
+          icon: 'account',
+          ref: '/account/profile'
+        }
+      ]
+    }
+  },
+  methods: {
+    goToApp (ref) {
+      this.$router.push({ path: ref })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/frontend/views/authenticated/account-profile.vue
@@ -0,0 +1,182 @@
+<template>
+  <div>
+    <div class="container">
+      <section class="section">
+        <nav class="breadcrumb is-medium has-arrow-separator" aria-label="breadcrumbs">
+            <ul>
+              <li><router-link to="/account">My account</router-link></li>
+              <li class="is-active"><router-link to="/account/profile">Profile</router-link></li>
+            </ul>
+        </nav>
+        <h1 class="title is-1">Profile</h1>
+        <p class="subtitle is-3">Manage your accounts</p>
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <figure class="image is-128x128">
+                  <img src="https://bulma.io/images/placeholders/256x256.png" alt="Placeholder image">
+                </figure>
+                <br/>
+                <p class="title is-3">{{ names }}</p>
+                <p class="subtitle is-5">Joined {{ TimestampToCalendar(creationTimestamp) }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+
+        <b-field grouped group-multiline>
+          <div class="control" v-for="group in groups" v-bind:key="group">
+            <b-taglist attached>
+              <b-tag type="is-dark">is</b-tag>
+              <b-tag type="is-info">{{ group }}</b-tag>
+            </b-taglist>
+          </div>
+        </b-field>
+        <br />
+
+        <b-field label="Name(s)">
+          <b-input type="text"
+                   v-model="names"
+                   maxlength="60"
+                   placeholder="Enter your name(s)"
+                   icon="textbox"
+                   size="is-medium"
+                   required>
+          </b-input>
+        </b-field>
+
+        <b-field label="Email">
+          <b-input type="email"
+                   v-model="email"
+                   maxlength="70"
+                   icon="email"
+                   size="is-medium"
+                   required>
+          </b-input>
+        </b-field>
+        <b-field label="Phone number">
+          <b-input type="tel"
+                   v-model="phoneNumber"
+                   placeholder="Enter your phone number"
+                   icon="phone"
+                   size="is-medium"
+                   maxlength="30">
+          </b-input>
+        </b-field>
+
+        <b-field label="Birthday">
+          <b-datepicker
+            v-model="jsBirthday"
+            :max-date="maxDate"
+            :min-date="minDate"
+            :show-week-numbers="true"
+            :focused-date="focusedDate"
+            placeholder="Click to select birthday"
+            icon="cake-variant"
+            size="is-medium"
+            trap-focus>
+          </b-datepicker>
+        </b-field>
+        <br/>
+
+        <b-field label="Password">
+          <b-input
+            type="password"
+            v-model="password"
+            password-reveal
+            placeholder="Enter to update your password"
+            icon="textbox-password"
+            size="is-medium"
+            maxlength="70">
+          </b-input>
+        </b-field>
+        <b-field label="Confirm password">
+          <b-input
+            type="password"
+            v-model="passwordConfirm"
+            password-reveal
+            placeholder="Confirm to update your password"
+            maxlength="70"
+            size="is-medium"
+            icon="textbox-password">
+          </b-input>
+        </b-field>
+        <b-button
+          type="is-success"
+          size="is-medium"
+          rounded
+          native-type="submit"
+          @click="PatchProfile(names, email, phoneNumber, password, passwordConfirm, jsBirthday)">
+          Update profile
+        </b-button>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import common from '@/frontend/common/common'
+import profile from '@/frontend/requests/authenticated/profile'
+
+export default {
+  name: 'profile',
+  data () {
+    const today = new Date()
+    const maxDate = new Date(today.getFullYear() - 15, today.getMonth(), today.getDate())
+    const minDate = new Date(today.getFullYear() - 100, today.getMonth(), today.getDate())
+
+    return {
+      maxDate: maxDate,
+      minDate: minDate,
+      focusedDate: maxDate,
+      passwordConfirm: '',
+      jsBirthday: null,
+      names: '',
+      email: '',
+      phoneNumber: '',
+      groups: [],
+      password: '',
+      birthday: 0,
+      creationTimestamp: ''
+    }
+  },
+  methods: {
+    GetProfile () {
+      profile.GetProfile().then(resp => {
+        this.names = resp.data.spec.names
+        this.birthday = resp.data.spec.birthday || null
+        this.jsBirthday = this.birthday !== null ? new Date(this.birthday * 1000) : null
+        this.focusedDate = this.jsBirthday
+        this.phoneNumber = resp.data.spec.phoneNumber
+        this.groups = resp.data.spec.groups
+        this.email = resp.data.spec.email
+        this.creationTimestamp = resp.data.spec.creationTimestamp
+      })
+    },
+    PatchProfile (names, email, phoneNumber, password, passwordConfirm, jsBirthday) {
+      if (password !== passwordConfirm) {
+        common.DisplayFailureToast('Unable to use password as they either do not match')
+        return
+      }
+      var birthday = new Date(jsBirthday || 0).getTime() / 1000 || 0
+      profile.PatchProfile(names, email, phoneNumber, birthday, password).then(resp => {
+        if (resp.data.spec.id === '') {
+          common.DisplayFailureToast('Failed to update profile')
+          return
+        }
+        common.DisplaySuccessToast('Successfully updated your profile')
+      }).catch(err => {
+        common.DisplayFailureToast('Failed to update profile' + '<br/>' + err.response.data.metadata.response)
+      })
+    },
+    TimestampToCalendar (timestamp) {
+      return common.TimestampToCalendar(timestamp)
+    }
+  },
+  async beforeMount () {
+    this.GetProfile()
+  }
+}
+</script>
