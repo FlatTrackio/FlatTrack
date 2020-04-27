f2587702f4901dfba86f8d49bd29295e1bd1790e
Caleb Woodbine <calebwoodbine.public@gmail.com>
2020-04-27 16:34:06 +1200

    Fix missing add buttons

---
+++ src/frontend/components/common/floating-add-button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="floating-add-button">
+    <md-speed-dial md-direction="bottom md-bottom-right">
+      <md-speed-dial-target @click="goToRef(path)" class="floating-add-button-colour">
+        <md-icon>add</md-icon>
+      </md-speed-dial-target>
+    </md-speed-dial>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'floating add button',
+  props: {
+    path: String
+  },
+  methods: {
+    goToRef (ref) {
+      this.$router.push({ path: ref })
+    }
+  }
+}
+</script>
+
+<style>
+.floating-add-button {
+    display: block;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    margin-bottom: 50px;
+}
+
+.floating-add-button-colour {
+    background-color: #a9e2f2;
+}
+</style>
