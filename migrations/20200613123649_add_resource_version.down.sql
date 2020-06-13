f3bbe903aaf4dd6a497b5a0cbf66591cdde5668c
Caleb Woodbine <calebwoodbine.public@gmail.com>
2020-06-13 15:35:02 +1200

    Fix down migration for resource version

--- migrations/20200613123649_add_resource_version.down.sql
+++ migrations/20200613123649_add_resource_version.down.sql
@@ -2,13 +2,13 @@ begin;
 
 -- TODO remove resource version
 
-alter table users drop column if exists resourceVersion int not null default 0;
-alter table groups drop column if exists resourceVersion int not null default 0;
-alter table settings drop column if exists resourceVersion int not null default 0;
-alter table shopping_item drop column if exists resourceVersion int not null default 0;
-alter table shopping_list drop column if exists resourceVersion int not null default 0;
-alter table system drop column if exists resourceVersion int not null default 0;
-alter table user_creation_secret drop column if exists resourceVersion int not null default 0;
-alter table user_to_groups drop column if exists resourceVersion int not null default 0;
+alter table users drop column if exists resourceVersion;
+alter table groups drop column if exists resourceVersion;
+alter table settings drop column if exists resourceVersion;
+alter table shopping_item drop column if exists resourceVersion;
+alter table shopping_list drop column if exists resourceVersion;
+alter table system drop column if exists resourceVersion;
+alter table user_creation_secret drop column if exists resourceVersion;
+alter table user_to_groups drop column if exists resourceVersion;
 
 commit;
