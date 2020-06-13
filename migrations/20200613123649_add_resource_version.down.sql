e3b078226f774795080ba6d93b1100a61e1f2180
Caleb Woodbine <calebwoodbine.public@gmail.com>
2020-06-13 15:26:11 +1200

    Fix migration keyword

--- migrations/20200613123649_add_resource_version.down.sql
+++ migrations/20200613123649_add_resource_version.down.sql
@@ -2,13 +2,13 @@ begin;
 
 -- TODO remove resource version
 
-alter table users delete column if exists resourceVersion int not null default 0;
-alter table groups delete column if exists resourceVersion int not null default 0;
-alter table settings delete column if exists resourceVersion int not null default 0;
-alter table shopping_item delete column if exists resourceVersion int not null default 0;
-alter table shopping_list delete column if exists resourceVersion int not null default 0;
-alter table system delete column if exists resourceVersion int not null default 0;
-alter table user_creation_secret delete column if exists resourceVersion int not null default 0;
-alter table user_to_groups delete column if exists resourceVersion int not null default 0;
+alter table users drop column if exists resourceVersion int not null default 0;
+alter table groups drop column if exists resourceVersion int not null default 0;
+alter table settings drop column if exists resourceVersion int not null default 0;
+alter table shopping_item drop column if exists resourceVersion int not null default 0;
+alter table shopping_list drop column if exists resourceVersion int not null default 0;
+alter table system drop column if exists resourceVersion int not null default 0;
+alter table user_creation_secret drop column if exists resourceVersion int not null default 0;
+alter table user_to_groups drop column if exists resourceVersion int not null default 0;
 
 commit;
