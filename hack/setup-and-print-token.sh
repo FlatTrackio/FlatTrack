69ecab0a60bc67461050da9616043a33c28b2ef6
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-05-25 20:47:27 +1200

    Use YAML file for setup-and-print-token.sh

--- hack/setup-and-print-token.sh
+++ hack/setup-and-print-token.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-LANGUAGE=${LANGUAGE:-English}
-TIMEZONE=${TIMEZONE:-Pacific/Auckland}
-FLATNAME=${FLATNAME:-Flat}
-USER_NAMES=${USER_NAMES:-Flatter}
-USER_EMAIL=${USER_EMAIL:-flattrack@example.com}
-USER_PASSWORD=${USER_PASSWORD:-P@ssw0rd123!}
+SCRIPT_PATH=$(dirname $(realpath $0))
+export LANGUAGE="${LANGUAGE:-English}"
+export TIMEZONE="${TIMEZONE:-Pacific/Auckland}"
+export FLATNAME="${FLATNAME:-Flat}"
+export USER_NAMES="${USER_NAMES:-Flatter}"
+export USER_EMAIL="${USER_EMAIL:-flattrack@example.com}"
+export USER_PASSWORD="${USER_PASSWORD:-P@ssw0rd123!}"
 
 token=$(curl \
     -H "Accept: application/json" \
     -X POST \
     http://localhost:8080/api/admin/register \
-    -d "{
-\"language\": \"$LANGUAGE\",
-\"timezone\": \"$TIMEZONE\",
-\"flatName\": \"$FLATNAME\",
-\"user\": {
-    \"names\": \"$USER_NAMES\",
-    \"email\": \"$USER_EMAIL\",
-    \"password\": \"$USER_PASSWORD\"
-  }
-}" 2> /dev/null | jq -r .data 2>&1)
+    -d "$(envsubst < $SCRIPT_PATH/defaultSetup.yaml | yq e -j -)" 2> /dev/null | jq -r .data 2>&1)
 
 if [ ! $? -eq 0 ]; then
     echo "failed to register" > /dev/stderr
