4d986e4544ea7c2d4188a3f500a6f41441f724e6
Caleb Woodbine <calebwoodbine.public@gmail.com>
2023-05-22 17:23:17 +1200

    Update build-container script

--- hack/build-container.sh
+++ hack/build-container.sh
@@ -4,49 +4,31 @@ function requiredEnv {
   name="${1}"
   value="${!1}"
   if [ -z "${value}" ]; then
-    echo "error: '${name}' is required to be set" > /dev/stderr
+    echo "error: '${name}' is required to be set" >/dev/stderr
     exit 1
   fi
 }
-
-function ko-publish-development {
+function ko-publish {
   ko publish \
     --jobs 100 \
     --bare \
-    --platform all \
-    --local \
-    .
-}
-
-function ko-publish-production {
-  ko publish \
-    --push \
-    --jobs 100 \
-    --bare \
-    --platform all \
-    --tags "${IMAGE_DESTINATIONS}" \
     .
 }
 
 cd $(dirname $0)
 cd $(git rev-parse --show-toplevel)
 
-rm -r ./kodata
-mkdir -p ./kodata/{web/dist,}
-cp -r ./migrations ./kodata
+mkdir -p ./kodata/web
+(
+  cd web
+  npm run build
+)
 cp -r ./web/dist ./kodata/web
 
-requiredEnvs=(APP_BUILD_HASH APP_BUILD_DATE APP_BUILD_VERSION APP_BUILD_MODE)
-for env in ${requiredEnvs[*]}; do
-  requiredEnv "${env}"
-done
-if [ ! "${1}" = "production" ]; then
-  ko-publish-development
-  exit $?
-fi
-
-requiredEnvs=(KO_DOCKER_REPO IMAGE_DESTINATIONS)
-for env in ${requiredEnvs[*]}; do
-  requiredEnv "${env}"
-done
-ko-publish-production
+export \
+  APP_BUILD_HASH="$(git rev-parse HEAD)-dirty" \
+  APP_BUILD_DATE="$(date +%Y.%m.%d.%H%M)" \
+  APP_BUILD_VERSION="0.0.0" \
+  APP_BUILD_MODE="development" \
+  KO_DOCKER_REPO="${KO_DOCKER_REPO:-registry.gitlab.com/flattrack/flattrack}"
+ko-publish
