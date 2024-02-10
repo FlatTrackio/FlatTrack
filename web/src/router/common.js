62e1c6843342c4568327850e38658c55ebc81dda
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-02-11 08:58:56 +1300

    chore: stop login+setup redirect loop
    continous loop of the two routes

--- web/src/router/common.js
+++ web/src/router/common.js
@@ -1,11 +1,16 @@
 import common from '@/common/common'
 import cani from '@/requests/authenticated/can-i'
+import login from '@/requests/public/login'
 
 // requireAuthToken
 // given an no auth token redirect to the login page
 function requireAuthToken (to, from, next) {
   var authToken = common.GetAuthToken()
-  if (typeof authToken === 'undefined' || authToken === null || authToken === '') {
+  if (
+    typeof authToken === 'undefined' ||
+    authToken === null ||
+    authToken === ''
+  ) {
     next({ path: '/login', query: { redirect: to.fullPath } })
     return
   }
@@ -16,23 +21,32 @@ function requireAuthToken (to, from, next) {
 // given an auth token, redirect to the home page
 function requireNoAuthToken (to, from, next) {
   var authToken = common.GetAuthToken()
-  if (typeof authToken === 'undefined' || authToken === null || authToken === '') {
+  if (
+    typeof authToken === 'undefined' ||
+    authToken === null ||
+    authToken === ''
+  ) {
     next()
     return
   }
-  window.location.href = '/'
+  login.GetUserAuth(false).then(() => {
+    window.location.href = '/'
+  })
 }
 
 function requireGroup (to, from, next) {
-  cani.GetCanIgroup(to.meta.requiresGroup).then(resp => {
-    if (resp.data.data === true) {
-      next()
-    } else {
+  cani
+    .GetCanIgroup(to.meta.requiresGroup)
+    .then((resp) => {
+      if (resp.data.data === true) {
+        next()
+      } else {
+        next(from.path)
+      }
+    })
+    .catch(() => {
       next(from.path)
-    }
-  }).catch(() => {
-    next(from.path)
-  })
+    })
 }
 
 function isPublicRoute (to) {
