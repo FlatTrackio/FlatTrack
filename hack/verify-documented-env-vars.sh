7cd15645451ca65b174285eba114d5953c76c9db
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-07-24 12:42:47 +1200

    chore: add PORT alt var for APP_PORT
    add alternative environment variable for configuring the listening http port

--- hack/verify-documented-env-vars.sh
+++ hack/verify-documented-env-vars.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o nounset
 
 FAIL=false
-for VAR in $(grep 'return GetEnvOrDefault' internal/common/common.go | sed 's/.*("\(.*\)",.*/\1/g' | xargs) ; do
-    if ! grep -q "$VAR" ./docs/configuration.md; then
+for VAR in $(grep 'GetEnvOrDefault("' internal/common/common.go | sed 's/.*("\(.*\)",.*/\1/g' | xargs) ; do
+    if ! grep -q -E "\`$VAR\`" ./docs/configuration.md; then
         echo "$VAR not found"
         FAIL=true
     fi
