e368db1154fca44a5dd86cfa46427d4a536445e8
Caleb Woodbine <calebwoodbine.public@gmail.com>
2025-04-02 10:52:01 +0100

    feat: generate frontend and backend spdx SBOM
    use Syft to produce multi-language SBOM

--- hack/publish.sh
+++ hack/publish.sh
@@ -26,6 +26,7 @@ fi
 
 cd "$(git rev-parse --show-toplevel)" || exit 1
 
+LOCAL_SBOM_PATH="./tmp/sbom"
 KO_DOCKER_REPO="${KO_DOCKER_REPO:-registry.gitlab.com/flattrack/flattrack}"
 APP_BUILD_HASH="${APP_BUILD_HASH:-$(git rev-parse HEAD | cut -c -8)}"
 APP_BUILD_DATE="$(git show -s --format=%cd --date=format:'%Y.%m.%d.%H%M')"
@@ -33,9 +34,9 @@ APP_BUILD_VERSION="${APP_BUILD_VERSION:-0.0.0}"
 APP_BUILD_MODE="${APP_BUILD_MODE:-staging}"
 IMAGE_DESTINATIONS="latest,${APP_BUILD_HASH}"
 if [[ -n "${CI_COMMIT_TAG:-}" ]]; then
-  APP_BUILD_VERSION="${CI_COMMIT_TAG:-}"
-  APP_BUILD_MODE=production
-  IMAGE_DESTINATIONS="$APP_BUILD_VERSION"
+    APP_BUILD_VERSION="${CI_COMMIT_TAG:-}"
+    APP_BUILD_MODE=production
+    IMAGE_DESTINATIONS="$APP_BUILD_VERSION"
 fi
 echo "Commit made on '${APP_BUILD_DATE:-}'"
 
@@ -49,28 +50,30 @@ export KO_DOCKER_REPO \
 IMAGE="$(ko publish \
     --bare \
     --tags "${IMAGE_DESTINATIONS}" \
+    --sbom-dir "$LOCAL_SBOM_PATH" \
     $KO_FLAGS \
     .)"
 
 if [ "${SIGN:-}" = true ]; then
-    cosign sign --recursive -y "$IMAGE"
-    cosign download sbom "$IMAGE" >/tmp/sbom-spdx.json
-    cosign attest -y --recursive --predicate /tmp/sbom-spdx.json "$IMAGE"
+    syft . --output spdx-json=tmp/sbom/flattrack.spdx.json
 
-    DIGESTS="$(crane manifest "$IMAGE" | jq -r .manifests[].digest)"
+    cosign sign --recursive -y "$IMAGE"
+    # ko generates an SBOM for the container manifests
+    cosign attest -y --recursive --predicate "$LOCAL_SBOM_PATH/flattrack-index.spdx.json" "$IMAGE"
+
+    DIGESTS="$(crane manifest "$IMAGE" | jq -r '.manifests[].digest')"
     for DIGEST in $DIGESTS; do
-        cosign download sbom "$KO_DOCKER_REPO@$DIGEST" >/tmp/sbom-spdx-"$DIGEST".json
-        cosign attest -y --recursive --predicate /tmp/sbom-spdx-"$DIGEST".json "$KO_DOCKER_REPO@$DIGEST"
+        cosign attest -y --recursive --predicate "$LOCAL_SBOM_PATH/flattrack.spdx.json" "$KO_DOCKER_REPO@$DIGEST"
     done
 fi
 
 if [ "${TEST_TARBALL:-}" = true ]; then
-    crane registry serve --address :5001&
+    crane registry serve --address :5001 &
     REGPID=$!
     IMG=$(crane --insecure push /tmp/flattrack.tar localhost:5001/ft)
 
-    RESULT="$(crane export "$IMG" - | tar -tvf - \
-        | grep -E '(etc/passwd|usr/share/zoneinfo|etc/ssl/certs|ko-app/flattrack|ko/web/assets/.*\.js|ko/migrations/.*\.sql)')"
+    RESULT="$(crane export "$IMG" - | tar -tvf - |
+        grep -E '(etc/passwd|usr/share/zoneinfo|etc/ssl/certs|ko-app/flattrack|ko/web/assets/.*\.js|ko/migrations/.*\.sql)')"
     CODE=$?
     if [ ! $CODE -eq 0 ]; then
         echo "error: failed to build image correctly" >/dev/stderr
